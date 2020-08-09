e863a01b83680d42860f211b6a53c5e78398b87e
shedrachIkenna <shedrach686@gmail.com>
2020-08-09 23:42:18 +0100

    finished hamburger menu on header

---
+++ main.js
@@ -0,0 +1,7 @@
+
+openMenu = () => {
+    console.log('Menu Opened')
+    document.getElementById('hamburger').classList.toggle('changed');
+    document.getElementById('menu').classList.toggle('remove-menu');
+    document.getElementById('menu-bg').classList.toggle('change-bg');
+}
